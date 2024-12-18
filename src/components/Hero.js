b33d51d1ac78e9a555444373c105b93395299258
abdul-busybrain <awwalhajine@gmail.com>
2024-12-18 06:28:33 +0100

    Commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,20 @@
 import { Button } from "./Button";
+import aiAssistant from "../img/ai.jpg";
 
 // NOTE Hero Section
 export function Hero() {
   return (
     <section
       id="hero"
-      className="pt-32 pb-16 px-4 sm:pt-40 sm:pb-24 bg-blue-50"
+      className="pt-32 pb-16 px-4 sm:pt-40 sm:pb-24 bg-blue-50 relative min-h-screen"
+      style={{
+        backgroundImage: `url(${aiAssistant})`,
+        backgroundSize: "100% 100%",
+        backgroundPosition: "top center",
+        backgroundRepeat: "no-repeat",
+      }}
     >
-      <div className="max-w-7xl mx-auto text-center ">
+      <div className="max-w-7xl mx-auto text-center relative z-10 pt-16">
         <h1 className="text-5xl sm:text-5xl md:text-6xl font-bold text-gray-900 leading-tight">
           Streamlined <br className="hidden sm:block" /> Communication for
           <br className="hidden sm:block" /> Quick Shipping
